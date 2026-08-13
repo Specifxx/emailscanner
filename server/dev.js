3c65cc9733ed481b2bc432b19558bec80142d21f
Bill <99713756+Specifxx@users.noreply.github.com>
2026-08-14 08:48:31 +1000

    Add route for deleting an account

--- server/dev.js
+++ server/dev.js
@@ -13,6 +13,7 @@ const ROUTES = [
   ['/api/auth/callback', '../api/auth/callback.js'],
   ['/api/auth/logout', '../api/auth/logout.js'],
   ['/api/accounts/disconnect', '../api/accounts/disconnect.js'],
+    ['/api/account/delete', '../api/account/delete.js'],
   ['/api/me', '../api/me.js'],
   ['/api/scan', '../api/scan.js'],
 ]
